eabde34113b1091c8d082463d1c2d0de6befbea0
Hextar IT 02134 <hextarit02134@Hextars-MacBook-Air.local>
2026-05-10 10:05:36 +0800

    refactor: image error handling

--- app/Http/Controllers/BE/AnnouncementImageController.php
+++ app/Http/Controllers/BE/AnnouncementImageController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\AnnouncementImageUpdateStatusRequest;
 use App\Http\Resources\AnnouncementImageResource;
 use App\Models\AnnouncementImage;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 
 class AnnouncementImageController extends Controller
 {
@@ -17,20 +18,30 @@ class AnnouncementImageController extends Controller
 
     public function updateStatus(AnnouncementImageUpdateStatusRequest $request, string $uuid)
     {
-        $announcement_image = AnnouncementImage::findByUuid($uuid);
+        DB::beginTransaction();
 
-        if ($announcement_image->image_path && Storage::disk('public')->exists($announcement_image->image_path) && $request->is_active == StatusCodeConstants::INACTIVE)
-        {
-            Storage::disk('public')->delete($announcement_image->image_path);
+        try {
+            $announcement_image = AnnouncementImage::findByUuid($uuid);
+
+            if ($announcement_image->image_path && Storage::disk('public')->exists($announcement_image->image_path) && $request->is_active == StatusCodeConstants::INACTIVE)
+            {
+                Storage::disk('public')->delete($announcement_image->image_path);
+            }
+
+            $announcement_image->update([
+                'image_path' => null,
+                'is_active' => $request->is_active ? StatusCodeConstants::ACTIVE : StatusCodeConstants::INACTIVE,
+                'updated_by' => self::auth()->uuid,
+                'updated_at' => self::currentDateTime(),
+            ]);
+            
+            DB::commit();
+            
+            return self::response(new AnnouncementImageResource($announcement_image));
+
+        } catch (\Exception $exception) {
+            DB::rollback();
+            throw $exception;
         }
-
-        $announcement_image->update([
-            'image_path' => null,
-            'is_active' => $request->is_active ? StatusCodeConstants::ACTIVE : StatusCodeConstants::INACTIVE,
-            'updated_by' => self::auth()->uuid,
-            'updated_at' => self::currentDateTime(),
-        ]);
-        
-        return self::response(new AnnouncementImageResource($announcement_image));
     }
 }
--- app/Http/Controllers/BE/UpcomingEventImageController.php
+++ app/Http/Controllers/BE/UpcomingEventImageController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\UpcomingEventImageUpdateStatusRequest;
 use App\Http\Resources\UpcomingEventImageResource;
 use App\Models\UpcomingEventImage;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 
 class UpcomingEventImageController extends Controller
 {
@@ -17,20 +18,30 @@ class UpcomingEventImageController extends Controller
 
     public function updateStatus(UpcomingEventImageUpdateStatusRequest $request, string $uuid)
     {
-        $upcoming_event_image = UpcomingEventImage::findByUuid($uuid);
+        DB::beginTransaction();
         
-        if ($upcoming_event_image->image_path && Storage::disk('public')->exists($upcoming_event_image->image_path) && $request->is_active == StatusCodeConstants::INACTIVE)
-        {
-            Storage::disk('public')->delete($upcoming_event_image->image_path);
-        }
+        try {
+            $upcoming_event_image = UpcomingEventImage::findByUuid($uuid);
+            
+            if ($upcoming_event_image->image_path && Storage::disk('public')->exists($upcoming_event_image->image_path) && $request->is_active == StatusCodeConstants::INACTIVE)
+            {
+                Storage::disk('public')->delete($upcoming_event_image->image_path);
+            }
 
-        $upcoming_event_image->update([
-            'image_path' => null,
-            'is_active' => $request->is_active ? StatusCodeConstants::ACTIVE : StatusCodeConstants::INACTIVE,
-            'updated_by' => self::auth()->uuid,
-            'updated_at' => self::currentDateTime(),
-        ]);
-        
-        return self::response(new UpcomingEventImageResource($upcoming_event_image));
+            $upcoming_event_image->update([
+                'image_path' => null,
+                'is_active' => $request->is_active ? StatusCodeConstants::ACTIVE : StatusCodeConstants::INACTIVE,
+                'updated_by' => self::auth()->uuid,
+                'updated_at' => self::currentDateTime(),
+            ]);
+            
+            DB::commit();
+
+            return self::response(new UpcomingEventImageResource($upcoming_event_image));
+
+        } catch (\Exception $exception) {
+            DB::rollback();
+            throw $exception;
+        }
     }
 }
